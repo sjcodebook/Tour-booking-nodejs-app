cccd4639cd7233516055ffb45dd816451b3e5570
sj <jainsanmati846@gmail.com>
2019-08-14 18:46:25 +0530

    MVC refactor

---
+++ models/tourModels.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour name is required.']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour price is required.']
+  }
+});
+
+// eslint-disable-next-line new-cap
+const Tour = new mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  // eslint-disable-next-line no-console
+  .then(() => console.log('MongoDB connected'));
+
 // Server Started
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Server started');
+  // eslint-disable-next-line no-console
+  console.log(`Server started at ${port}`);
 });

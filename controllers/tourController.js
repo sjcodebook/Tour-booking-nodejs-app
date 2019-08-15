0ad3f9f92329fe3e131f3c495fe7ba000b528d35
sj <jainsanmati846@gmail.com>
2019-08-15 12:43:39 +0530

    Add tour update and delete route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -63,18 +63,37 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour: 'updated tour'
-  //   }
-  // });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  // res.status(204).json({
-  //   status: 'success',
-  //   data: null
-  // });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };

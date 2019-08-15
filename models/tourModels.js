4aa98d05881ae166f707e3b00564df61dd4dfc50
sj <jainsanmati846@gmail.com>
2019-08-15 17:16:52 +0530

    Update tour model

--- models/tourModels.js
+++ models/tourModels.js
@@ -4,18 +4,109 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'A tour name is required.'],
+    required: [true, 'A tour must have a name'],
     unique: true,
-    dropDups: true
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters']
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
   },
-  rating: {
+  slug: String,
+  duration: {
     type: Number,
-    default: 4.5
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+    set: val => Math.round(val * 10) / 10
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
-    required: [true, 'A tour price is required.']
-  }
+    required: [true, 'A tour must have a price']
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        // this only points to current doc on NEW document creation
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price'
+    }
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false
+  },
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
+  },
+  startLocation: {
+    // GeoJSON
+    type: {
+      type: String,
+      default: 'Point',
+      enum: ['Point']
+    },
+    coordinates: [Number],
+    address: String,
+    description: String
+  },
+  locations: [
+    {
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point']
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+      day: Number
+    }
+  ],
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ]
 });
 
 // eslint-disable-next-line new-cap

93acbd8dfcb9d83347b26cf6a50b2a0658733844
sj <jainsanmati846@gmail.com>
2019-08-16 12:45:30 +0530

    Add api alias

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('./../models/tourModels');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.sort = '-ratingsAverage,price';
+  req.query.limit = '5';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // Filtering
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const tourController = require('./../controllers/tourController');
 
 router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);

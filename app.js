26e2852b9cfd6d0e27f18b238bafc0e21314dc86
sj <jainsanmati846@gmail.com>
2019-08-30 23:45:31 +0530

    bug fix

--- app.js
+++ app.js
@@ -13,4 +13,24 @@ app.use(express.json());
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  const err = new Error(`can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+
+  next();
+});
+
 module.exports = app;

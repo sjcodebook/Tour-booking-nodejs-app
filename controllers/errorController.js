95a50b5e74c95ddf52b1991d10510c066442acee
sj <jainsanmati846@gmail.com>
2019-11-01 23:52:19 +0530

    error seperations

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,41 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  }
+  // Programming or other unknow error
+  else {
+    // eslint-disable-next-line no-console
+    console.error('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 
   next();
 };

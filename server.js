3b24d1964b99420fc76fe71ad7dd7fec5e4820a4
sj <jainsanmati846@gmail.com>
2019-11-05 03:25:02 +0530

    handling unhandled promise rejection

--- server.js
+++ server.js
@@ -18,7 +18,14 @@ mongoose
 
 // Server Started
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(process.env.NODE_ENV);
   console.log(`Server started at ${port}`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
